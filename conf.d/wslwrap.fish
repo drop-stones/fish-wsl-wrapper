0b7b655eda8754fee877ee8b27d482ac22123dff
drop-stones <s2h6i8z1u7k8u7@gmail.com>
2025-09-24 21:34:35 +0900

    feat(config): unregister all managed functions and remove variables when uninstalling

--- conf.d/wslwrap.fish
+++ conf.d/wslwrap.fish
@@ -2,3 +2,11 @@ function _wslwrap_install --on-event wslwrap_install --description "Install wslw
     # Marker used to identify functions managed by wslwrap.fish
     set --universal wslwrap_function_marker "wslwrap.fish::managed"
 end
+
+function _wslwrap_uninstall --on-event wslwrap_uninstall --description "Uninstall wslwrap by unregistering all managed functions"
+    # Unregister all managed functions
+    wslwrap list | wslwrap unregister
+
+    # Remove the universal variable
+    set -q wslwrap_function_marker && set --erase wslwrap_function_marker
+end
