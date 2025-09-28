0c6c50b80a1cd5785c8163667d40c6291c9d28e2
drop-stones <s2h6i8z1u7k8u7@gmail.com>
2025-09-28 13:57:26 +0900

    feat(completions): add add-path command completions

--- completions/wslwrap.fish
+++ completions/wslwrap.fish
@@ -1,10 +1,11 @@
 # All subcommands
-set -l commands register unregister list help
+set -l commands register unregister list add-path help
 
 # subcommands
 complete --command wslwrap --condition "not __fish_seen_subcommand_from $commands" --no-files --arguments register --description "Register a wrapper (auto/windows)"
 complete --command wslwrap --condition "not __fish_seen_subcommand_from $commands" --no-files --arguments unregister --description "Remove one or more registered wrappers"
 complete --command wslwrap --condition "not __fish_seen_subcommand_from $commands" --no-files --arguments list --description "List registered wrapper names"
+complete --command wslwrap --condition "not __fish_seen_subcommand_from $commands" --no-files --arguments add-path --description "Add Windows command directories to PATH"
 complete --command wslwrap --condition "not __fish_seen_subcommand_from $commands" --no-files --arguments help --description "Show general or command-specific help"
 
 # register: options
@@ -41,6 +42,22 @@ complete --command wslwrap --condition "_wslwrap_at 2 unregister" \
 
 # list: no arguments
 complete --command wslwrap --condition "_wslwrap_at 2 list" --no-files
+
+# add-path: options
+complete --command wslwrap --condition "_wslwrap_at 2 add-path" \
+    --short-option a \
+    --long-option append \
+    --description "Add to end of PATH (lower priority, default)"
+
+complete --command wslwrap --condition "_wslwrap_at 2 add-path" \
+    --short-option p \
+    --long-option prepend \
+    --description "Add to beginning of PATH (higher priority)"
+
+# add-path: no suggestions
+complete --command wslwrap --condition "_wslwrap_at 2 add-path" \
+    --no-files \
+    --description "Command to add to PATH"
 
 # help: suggest all subcommands
 complete --command wslwrap --condition "_wslwrap_at 2 help; and test (count (commandline -xpc)) -eq 2" \
