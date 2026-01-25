15e79d5c63c85272e031b3eb7a8885d21c472c26
drop-stones <s2h6i8z1u7k8u7@gmail.com>
2026-01-25 15:01:28 +0900

    fix(filesystem): fixed missed detection for '/mnt/[a-z]'

--- functions/_wslwrap_in_windows_filesystem.fish
+++ functions/_wslwrap_in_windows_filesystem.fish
@@ -1,3 +1,3 @@
 function _wslwrap_in_windows_filesystem --argument-names path --description 'Detect if current directory is in Windows filesystem (/mnt/[a-z]/)'
-    return (string match -qr '^/mnt/[a-z]/' -- $path)
+    return (string match -rq '^/mnt/[a-z](?:/|$)' -- $path)
 end
